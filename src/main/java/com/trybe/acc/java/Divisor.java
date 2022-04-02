872d7b967424fc49a11f6498ddfc59ab1060c8b8
ricardo-sousa <rwmsousa@gmail.com>
2022-04-02 12:11:03 -0300

    feat: resolution exercise

--- src/main/java/com/trybe/acc/java/Divisor.java
+++ src/main/java/com/trybe/acc/java/Divisor.java
@@ -1,5 +1,18 @@
 package com.trybe.acc.java;
 
-public class Divisor {
+// Escreva uma classe que possua um método que consiga dividir dois números inteiros. Você deverá
+// chamar a classe de Divisor, e o método, de dividir, que receberá dois argumentos do tipo int e
+// também deverá retornar um int.
 
+// Dentro desse método, você deverá fazer o tratamento do erro ArithmeticException, garantindo que
+// se, por exemplo, forem passados os números 2 e 0, o método consiga ser executado com sucesso e,
+// em caso de erro, ele retorne o número 0.
+public class Divisor {
+  public int dividir(int a, int b) {
+    try {
+      return a / b;
+    } catch (ArithmeticException e) {
+      return 0;
+    }
+  }
 }
